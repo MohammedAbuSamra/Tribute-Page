34138578ddcf45be456a492dcc5475dd970d627d
MohammedAbuSamra <abusamramhmd@gmail.com>
2022-07-22 19:52:29 +0300

    fix header

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 let dateEl = document.getElementById("date-el");
 
-const date = new Date();
-const day = date.getDate();
-const month = date.getMonth();
-const year = date.getFullYear();
-
-const currentDate = `${day}/${month}/${year}`;
+const date = new Date(Date.now());
+const currentDate = date.toLocaleString();
+console.log(date.toLocaleString());
 
 dateEl.textContent = currentDate;
